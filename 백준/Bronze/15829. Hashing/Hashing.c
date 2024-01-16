b3e7f56ac97fecd22032dac7f7cd3d57342ad88d
zserty12 <126051673+zserty12@users.noreply.github.com>
2024-01-16 13:49:57 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	int L, r = 31, M = 1234567891, res = 0;
+	char str[51];
+	int i, j;
+	scanf("%d %s", &L, str);
+	for (i = 0; i < L; i++) {
+		int tmp = 1;
+		for (j = 0; j < i; j++) tmp *= r;
+		res += (str[i] - 96) * tmp;
+	}
+	int mod = res / M;
+	if (mod > 0) res -= M * mod;
+	printf("%d\n", res);
+	return 0;
+}
