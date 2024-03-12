66c212861cbb03c7e146a315fb6de53c7627c1a4
zserty12 <126051673+zserty12@users.noreply.github.com>
2024-03-12 20:16:54 +0900

    [Silver IV] Title: ATM, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,11 +1,7 @@
 N = int(input())
 num = list(map(int, input().split()))
 num.sort()
-min = [0]*N
-for i in range(0,N):
-    for j in range(0,i+1) :
-        min[i] += num[j]
 min_val = 0
-for k in range(0,N):
-    min_val += min[k]
+for k in range(1,N+1):
+    min_val += sum(num[0:k])
 print(min_val)
