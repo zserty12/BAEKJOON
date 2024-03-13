d9de71c7a271f9fdde5d737372bc4045bae45319
zserty12 <126051673+zserty12@users.noreply.github.com>
2024-03-13 14:30:54 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 220 ms, Memory: 49040 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,11 +1,9 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 dic = {}
 for i in range(0,N):
     tmpn, tmpm = input().split()
     dic[tmpn] = tmpm
-password = []
 for j in range(0,M):
-    tmppass = input()
-    password.append(tmppass)
-for j in range(0,M):
-    print(dic[password[j]])
+    print(dic[input().rstrip()])
