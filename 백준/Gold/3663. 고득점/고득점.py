b42b5fad410c87f75e4cb0e1c031b16c1afee8ef
zserty12 <126051673+zserty12@users.noreply.github.com>
2024-07-24 12:45:34 +0900

    [Gold III] Title: 고득점, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/3663. 고득점/고득점.py
@@ -0,0 +1,13 @@
+N = int(input())
+for _ in range(N):
+    name = input()
+    spell_cnt = 0
+    pos_cnt = len(name)-1
+    for i, spell in enumerate(name):
+        spell_cnt += min(ord(spell)-ord('A'), ord('Z')-ord(spell)+1)
+        
+        idx = i+1
+        while idx < len(name) and name[idx] == 'A':
+            idx += 1
+        pos_cnt = min([pos_cnt,2*i+len(name)-idx,2*(len(name)-idx)+i])
+    print(spell_cnt+pos_cnt)
